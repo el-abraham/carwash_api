fe599bbcd55c79d812a511e1ffec82b80aba8787
Dionisius951 <dionisius678@gmail.com>
2025-01-23 09:53:10 +0700

    update index

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 4000;
 const RoleRouter = require("./routes/RoleRouter");
 const UserRouter = require("./routes/userRouter");
 const bookingRouter = require("./routes/bookingRouter");
@@ -22,5 +21,5 @@ app.use("/notification", notificationRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log("Server Running At PORT : " + port);
+  console.log("Server Running At PORT : " + PORT);
 });
