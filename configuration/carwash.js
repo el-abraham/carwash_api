cbfbb210e5452301d65fdf0d999d877b5fef2bc9
Daniel Abraham <el.abraham1412@gmail.com>
2025-01-25 13:06:00 +0700

    config

--- configuration/carwash.js
+++ configuration/carwash.js
@@ -1,11 +1,13 @@
 const mysql = require("mysql2/promise");
-require("dotenv").config()
+require("dotenv").config();
 
+// const carwash = mysql.createPool()
 const carwash = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 module.exports = carwash;
